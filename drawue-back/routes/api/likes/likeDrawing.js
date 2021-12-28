48519fb102d64f952c6fcf3b327c5c8d0e43c9f1
Laurynas Leišis <lauris.leisis@gmail.com>
2021-12-28 22:20:51 +0200

    Refactored likeDrawing.js

--- drawue-back/routes/api/likes/likeDrawing.js
+++ drawue-back/routes/api/likes/likeDrawing.js
@@ -25,18 +25,7 @@ function dislikeLikedPost(req, res) {
     let post_id;
     if (ObjectID.isValid(req.body.postID)) post_id = req.body.postID.toString();
     else res.status(400).send({'status': 'error', 'message': 'Invalid post ID'});
-    Drawings.findOneAndUpdate({_id: post_id}, {$inc: { like_count: -1 }}, (drawing_error, response)=>{
-        if(drawing_error) console.log(drawing_error);
-        else{
-            if(req.body.author_username){
-                User.findOneAndUpdate({username: req.body.author_username}, {$inc: { total_like_count: -1 }}, (user_error, user_response)=>{
-                    if(user_error) console.log(user_error);
-                    else return res.status(200).send({'status':false});
-                });
-            }
-            else return res.status(200).send({'status':false});
-        }
-    });
+    updateLikeStatus(req, res, post_id, -1);
 }
 function likeDrawing(req, res) {
     let like = new Likes({
@@ -47,21 +36,24 @@ function likeDrawing(req, res) {
     if (ObjectID.isValid(req.body.postID)) post_id = req.body.postID.toString();
     else res.status(400).send({'status': 'error', 'message': 'Invalid post ID'});
     like.save().then(()=>{
-        Drawings.findOneAndUpdate({_id: post_id}, {$inc: { like_count: 1 }}, (drawing_error, drawing)=>{
-            if(drawing_error) console.log(drawing_error);
-            else {
-                if(req.body.author_username){
-                    User.findOneAndUpdate({username: req.body.author_username}, {$inc: { total_like_count: 1 }}, (user_error, user)=>{
-                        if(user_error) console.log(user_error);
-                        else return res.status(200).send({'status':true});
-                    });
-                }
-                else return res.status(200).send({'status':true});
-            }
-        });
+       updateLikeStatus(req, res, post_id, 1);
     })
     .catch((like_error)=>{
         console.log(like_error);
     })
+}
+function updateLikeStatus(req, res, post_id, status_value) {
+    Drawings.findOneAndUpdate({_id: post_id}, {$inc: { like_count: status_value }}, (drawing_error, drawing)=>{
+        if(drawing_error) console.log(drawing_error);
+        else {
+            if(req.body.author_username){
+                User.findOneAndUpdate({username: req.body.author_username}, {$inc: { total_like_count: status_value }}, (user_error, user)=>{
+                    if(user_error) console.log(user_error);
+                    else return res.status(200).send({'status':true});
+                });
+            }
+            else return res.status(200).send({'status':true});
+        }
+    });
 }
 module.exports = router;
